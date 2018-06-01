44dbeb630dfd76572113b2826b37c354b6e52ae5
Torre <torre.locascio@gmail.com>
2018-06-01 16:12:00 -0400

    FB Login Button finally appearing

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FBLoginButton from './FBLoginButton'
 
 export default class App extends React.Component {
   render() {
@@ -8,7 +9,12 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.container}>
+        <Text style={styles.label}>Welcome to the Facebook SDK for React Native!</Text>
+        <FBLoginButton />
       </View>
+      </View>
+
     );
   }
 }
@@ -16,8 +22,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    marginBottom: 48,
   },
 });
+
